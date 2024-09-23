e5584ad704854fd0907bd28393a6735ded84124a
Growl Kat <offmoney9@gmail.com>
2024-09-22 20:51:16 -0600

    [FIX] Documentation Issue

--- SF_Lang_Dictionary.Controllers/Program.cs
+++ SF_Lang_Dictionary.Controllers/Program.cs
@@ -127,13 +127,9 @@ builder.Services.AddSwaggerGen(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
-{
-    
-}*/
-
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.UseCors();
 app.UseAuthentication();
